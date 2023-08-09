6a25ef84ffd15d75dad51c52a2495e9a7a262ba6
TimmyMuema <muematimmy@gmail.com>
2023-08-09 05:27:22 -0700

    1-edges.py

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,6 +1,7 @@
 def first_three(word): return word[:3]
 def last_two(word): return word[-2:]
 def middle(word): return word[1:-1]
-print(f"First 3 letters: {first_three('Holberton')}")
-print(f"Last 2 letters: {last_two('Holberton')}")
-print(f"Middle word: {middle('Holberton')}")
+print(f"First 3 letters: {first_three('Schhool')}")
+print(f"Last 2 letters: {last_two('Schhool')}")
+print(f"Middle word: {middle('Schhool')}")
+
