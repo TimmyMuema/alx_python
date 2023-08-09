5e25e76e810209526fb4f7b9759a2efd4db3a056
TimmyMuema <muematimmy@gmail.com>
2023-08-09 06:36:40 -0700

    6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def combinations(i, j):
     if i < j:
-        return '{}{}'.format(i, j)
+        return str(i) + str(j)
     else:
         return ''
 
 print(', '.join([combinations(i, j) for i in range(10) for j in range(10)]))
+
