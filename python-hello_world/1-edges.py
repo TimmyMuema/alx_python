3748148461fe49f6123aa2ec17ef5d6c50739483
TimmyMuema <muematimmy@gmail.com>
2023-08-09 05:22:39 -0700

    1-edges.py

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,19 +1,6 @@
-#!/usr/bin/python3
-def get_first_three_letters(word):
-    return word[:3]
-
-def get_last_two_letters(word):
-    return word[-2:]
-
-def get_middle_word(word):
-    return word[1:-1]
-
-word = "Holberton"
-
-word_first_3 = get_first_three_letters(word)
-word_last_2 = get_last_two_letters(word)
-middle_word = get_middle_word(word)
-
-print("First 3 letters:", word_first_3)
-print("Last 2 letters:", word_last_2)
-print("Middle word:", middle_word)
+def first_three(word): return word[:3]
+def last_two(word): return word[-2:]
+def middle(word): return word[1:-1]
+print(f"First 3 letters: {first_three('Holberton')}")
+print(f"Last 2 letters: {last_two('Holberton')}")
+print(f"Middle word: {middle('Holberton')}")
