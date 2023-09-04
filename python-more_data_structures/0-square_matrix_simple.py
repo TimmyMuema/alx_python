fe4b60bd7f73f26f955bd0672f474d0528c0706d
TimmyMuema <muematimmy@gmail.com>
2023-09-04 10:27:12 -0700

    1-common_elements.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,6 @@
-def square_matrix_lambda(matrix=[]):
-    return [[x ** 2 for x in row] for row in matrix]
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        new_row = [x ** 2 for x in row]
+        new_matrix.append(new_row)
+    return new_matrix
---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,2 @@
+def common_elements_list_comprehension(set_1, set_2):
+    return {element for element in set_1 if element in set_2}
