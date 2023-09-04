3ee906d5721149a0592b376ce5fba4409b0ed112
TimmyMuema <muematimmy@gmail.com>
2023-09-03 23:02:59 -0700

    1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -5,12 +5,9 @@ def print_arguments():
 
     argc = len(sys.argv[1:])
 
-    if argc == 0:
-        print("{} argument(s).\n".format(argc))
-    else:
-        print("{} argument(s):\n".format(argc))
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print("{}: {}".format(i, arg))
+    print("{} argument(s):\n".format(argc))
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, arg))
 
 if __name__ == "__main__":
     print_arguments()
