d3788221a573bf0168870dac4ad92fdbfec23183
TimmyMuema <muematimmy@gmail.com>
2023-09-06 04:28:06 -0700

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,15 @@
+# Module documentation
+"""
+This module defines a Square class with a private size attribute.
+"""
+
+# Square class
+class Square:
+
+    # Private size attribute
+    __size = None
+
+    # Class constructor
+    def __init__(self, size):
+        # Set the size attribute
+        self.__size = size
