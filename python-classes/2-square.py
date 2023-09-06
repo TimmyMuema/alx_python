fa024b493191144f19f242537611e3b2545cd7be
TimmyMuema <muematimmy@gmail.com>
2023-09-06 04:48:38 -0700

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,24 +1,17 @@
+#!/usr/bin/python3
+"""Module documentation"""
+
 class Square:
-    """Square class with private size attribute."""
+    """Square class with private size attribute"""
 
     def __init__(self, size=0):
-        """
-        Class constructor.
-
-        Args:
-            size (int, optional): The size of the square. Defaults to 0.
-
-        Raises:
-            TypeError: If `size` is not an integer.
-            ValueError: If `size` is less than 0.
-        """
-        self.size = size  # Public size attribute
-
+        """Class constructor"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
         """Calculate and return the area of the square"""
-        return self.size ** 2
+        return self.__size ** 2
