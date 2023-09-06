cbc4c930c6e2f3201327f61624f72a0402431379
TimmyMuema <muematimmy@gmail.com>
2023-09-06 05:21:34 -0700

    1-is_kind_of_class.py

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,7 @@
+def is_kind_of_class(obj, a_class):
+    if isinstance(obj, a_class):
+        return True
+    elif issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
